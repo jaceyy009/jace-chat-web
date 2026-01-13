44233d5939ddbffef72af248fc060011301b4f4e
jaceyy009 <120229247+jaceyy009@users.noreply.github.com>
2026-01-13 16:14:30 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+// ap.js
+
+// Initialize Firebase (uses firebaseConfig from config.js)
+const app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.ath();
+const db = firebase.firestore();
+
+console.log("Firebase initialized!");
