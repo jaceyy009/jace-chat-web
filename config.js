f7096b43490e344dac4b8df0072d354071ea3e0a
jaceyy009 <120229247+jaceyy009@users.noreply.github.com>
2026-01-13 16:10:50 -0500

    Add Firebase configuration to config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDW3G9KqiOS_h1-cGoQ7hLiD0RR1oeKGEc",
+  authDomain: "jacechatweb.firebaseapp.com",
+  projectId: "jacechatweb",
+  storageBucket: "jacechatweb.firebasestorage.app",
+  messagingSenderId: "819081174442",
+  appId: "1:819081174442:web:6dfca40248b4d82d7a9fd3"
+};
